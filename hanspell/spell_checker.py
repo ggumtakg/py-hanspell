7c6bc733ad236f110db019654a12964d10791390
ggumtakg <44538954+ggumtakg@users.noreply.github.com>
2024-12-03 12:40:23 +0900

    Update spell_checker.py

--- hanspell/spell_checker.py
+++ hanspell/spell_checker.py
@@ -47,11 +47,12 @@ def check(text):
     passport_key = get_passport_key()
 
     # 페이로드를 브라우저에서 직접 확인한 값으로 변경함.
+    # passportKey와 _를 직접 수정해서 사용하세요.
     payload = {
         'passportKey': passport_key,
         'where': 'nexearch',
         'color_blindness': '0',
-        #'_': '1733194059596',
+        '_': '1733194059596',
         'q': text
     }
 
